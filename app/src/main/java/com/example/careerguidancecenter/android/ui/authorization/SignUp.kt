4427d7e7530647264aae70aab2946257777866f6
Booravchique <brvchq@gmail.com>
2022-05-15 18:14:20 +0500

    Экраны регистрации и входа
    переходы между ними

--- app/src/main/java/com/example/careerguidancecenter/android/ui/authorization/SignUp.kt
+++ app/src/main/java/com/example/careerguidancecenter/android/ui/authorization/SignUp.kt
@@ -1,116 +1,186 @@
 package com.example.careerguidancecenter.android.ui.authorization
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Green
+import androidx.compose.ui.graphics.Color.Companion.Transparent
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.example.careerguidancecenter.android.network.model.ServiceResultGeneric
 import com.example.careerguidancecenter.android.network.model.SignResult
 import com.example.careerguidancecenter.android.network.model.SignUpInfo
 import com.example.careerguidancecenter.android.ui.Nav
-import com.example.careerguidancecenter.android.ui.theme.BackgroundFillGray
-
+import com.example.careerguidancecenter.android.ui.theme.*
 
 @Composable
 fun SignUp(
     navHostController: NavHostController,
     viewModel: SignViewModel
-){
+) {
+    val shape = RoundedCornerShape(10.dp)
+
     var fullName = remember { mutableStateOf("") }
-    var email = remember { mutableStateOf("") }
     var password = remember { mutableStateOf("") }
+
+    val constraints = ConstraintSet {
+        val firstChild = createRefFor("firstChild")
+        val secondChild = createRefFor("secondChild")
+
+        constrain(firstChild){
+            top.linkTo(parent.top)
+            bottom.linkTo(parent.bottom)
+        }
+
+        constrain(secondChild){
+            bottom.linkTo(parent.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+    }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(BackgroundFillGray)
-            .padding(end = 15.dp, start = 15.dp)
+            .padding(end = 16.dp, start = 16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .fillMaxSize()
-                .background(BackgroundFillGray)
-        )
-        {
-            Column() {
+        ConstraintLayout(
+            constraints,
+        modifier = Modifier.fillMaxSize()
+        ) {
+            Column(
+                modifier = Modifier.layoutId("firstChild"),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    modifier = Modifier.padding(bottom = 16.dp),
+                    text = "ВХОД",
+                    color = DarkTextColor,
+                    fontFamily = RalewayFontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 38.sp,
+                    textAlign = TextAlign.Center
+                )
                 OutlinedTextField(
-
-                    value =  fullName.value,
+                    value = fullName.value,
                     modifier = Modifier
-                        .fillMaxWidth(),
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 4.dp),
                     onValueChange = { fullName.value = it },
-                    label = { Text("ФИО") },
-                    singleLine = true
-
+                    placeholder = { Text("Имя") },
+                    singleLine = true,
+                    shape = shape,
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = BorderCyan,
+                        unfocusedBorderColor = BorderGray,
+                        backgroundColor = White
+                    )
                 )
-
                 OutlinedTextField(
-
-                    value =  email.value,
+                    value = password.value,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(bottom = 10.dp),
-                    onValueChange = { email.value = it },
-                    label = { Text("Почта") },
-                    singleLine = true
-
-                )
-
-                OutlinedTextField(
-
-                    value =  password.value,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 10.dp),
+                        .padding(horizontal = 16.dp, vertical = 4.dp),
                     onValueChange = { password.value = it },
-                    label = { Text("Пароль") },
-                    singleLine = true
-
+                    placeholder = { Text("Пароль") },
+                    singleLine = true,
+                    shape = shape,
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = BorderCyan,
+                        unfocusedBorderColor = BorderGray,
+                        backgroundColor = White
+                    )
                 )
-
                 OutlinedButton(
-
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 10.dp),
+                        .padding(top = 30.dp),
                     onClick = {
                         var result = viewModel.SignUp(
                             SignUpInfo(
                                 fullName.value,
-                                email.value,
-                                password.value
+                                password.value,
                             )
                         )
-
-                        if(result.Success){
+                        if (result.Success) {
                             navHostController.navigate(Nav.Home.route)
                         }
                     },
+                    shape = shape,
+                    colors = ButtonDefaults.outlinedButtonColors(
+                        backgroundColor = MainCyan
+                    ),
+                    border = BorderStroke(1.dp, BorderCyan),
 
-                )
+                    )
                 {
-                    Text("Отправить")
+                    Text(
+                        text = "ВОЙТИ",
+                        fontFamily = RalewayFontFamily,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp,
+                        color = White,
+                        textAlign = TextAlign.Center
+                    )
                 }
+                OutlinedButton(
+                    modifier = Modifier.padding(top = 8.dp),
+                    onClick = {
 
+                    },
+                    colors = ButtonDefaults.outlinedButtonColors(
+                        backgroundColor = Transparent
+                    ),
+                    border = BorderStroke(0.dp, Transparent),
+                ) {
+                    Text(
+
+                        text = "Забыли пароль?",
+                        color = MainGray
+                    )
+                }
             }
 
+            OutlinedButton(
+                modifier = Modifier.padding(top = 8.dp).layoutId("secondChild"),
+                onClick = {
+                    navHostController.navigate(Nav.SignIn.route)
+                },
+                colors = ButtonDefaults.outlinedButtonColors(
+                    backgroundColor = Transparent
+                ),
+                border = BorderStroke(0.dp, Transparent),
+            ) {
+                Text(
+                    text = "У вас нет учетной записи? Создайте ее!",
+                    color = MainGray
+                )
+            }
         }
-
-
     }
-
-
 }
+
