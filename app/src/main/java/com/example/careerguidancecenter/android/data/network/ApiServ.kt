319120b0a8dc77ba953f9f37bddd89bd454f0abf
R0ge <nagaev.egor@mail.ru>
2022-05-15 19:11:27 +0500

    viewModels

--- app/src/main/java/com/example/careerguidancecenter/android/data/network/ApiServ.kt
+++ app/src/main/java/com/example/careerguidancecenter/android/data/network/ApiServ.kt
@@ -2,11 +2,12 @@ package com.example.careerguidancecenter.android.data.network
 
 import com.example.careerguidancecenter.android.domain.models.SignUpBackResult
 import retrofit2.http.Body
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface ApiServ {
 
 
     @POST("Authorization/SignUp")
-    fun postSignUn(@Body hashMap: HashMap<String,String>): SignUpBackResult
+       suspend  fun postSignUn( @Body hashMap: HashMap<String,String>): SignUpBackResult
 }
